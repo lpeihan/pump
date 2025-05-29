dfa14b7ab5bb2521bcb721006d0fb298358a62d1
Alfred Li <Alfred.Li@ringcentral.com>
2025-05-29 16:59:07 +0800

    feat: update

--- README.md
+++ README.md
@@ -17,13 +17,7 @@ pnpm run lint
 pnpm run deploy
 ```
 
-## tools
+##
 
-```shell
-# Icons
-https://free-icons.github.io/free-icons/
-
-# Deploy to Netlify
-https://www.netlify.com/
-
-```
+1. 需要先创建一个 token 作为 usdtMint，将 src/web3/index.ts 40 行改为创建的 usdtMint
+2. 调用 buyTokens 如果失败，需要往 src/web3/index.ts 372 行打印的 pda 地址打 1 个 SOL
--- src/web3/index.ts
+++ src/web3/index.ts
@@ -38,12 +38,10 @@ export const config = {
 
   programId: '8u2V6SHBURgDV23rvWFKBvPvhthYKP3eHfYgGJzQHLps',
   usdtMint: 'EsZS11jSoHqDuxNtjqrSuHgiCVUcck7UbNwcpgmarNox',
-  btcMint: '9H6VhHiCJ4C7aXGASTyd7vjYy9b7eMTjzJH83y2YCodF',
 };
 
 export const PROGRAM_ID = new PublicKey(config.programId);
 export const USDT_MINT = new PublicKey(config.usdtMint);
-export const BTC_MINT = new PublicKey(config.btcMint);
 
 const connection = new Connection(config.rpcUrl, 'confirmed');
 let authority: PublicKey;
@@ -320,18 +318,20 @@ export const createToken = async (
   return mintKeypair.publicKey.toString();
 };
 
-export const fetchSaleAccount = async () => {
+export const fetchSaleAccount = async (mint) => {
+  const tokenMint = new PublicKey(mint);
   const [saleAccountInstance] = PublicKey.findProgramAddressSync(
-    [Buffer.from('token_sale'), BTC_MINT.toBuffer()],
+    [Buffer.from('token_sale'), tokenMint.toBuffer()],
     PROGRAM_ID,
   );
 
   return accountFetch('saleAccount', saleAccountInstance);
 };
 
-export const fetchUserPurchase = async () => {
+export const fetchUserPurchase = async (mint) => {
+  const tokenMint = new PublicKey(mint);
   const [userPurchaseInstance] = PublicKey.findProgramAddressSync(
-    [Buffer.from('token_purchase'), authority.toBuffer(), BTC_MINT.toBuffer()],
+    [Buffer.from('token_purchase'), authority.toBuffer(), tokenMint.toBuffer()],
     PROGRAM_ID,
   );
 
@@ -368,12 +368,8 @@ export const buyTokens = async (mint, buyAmount) => {
   try {
     const tokenMint = new PublicKey(mint);
     const transaction = new Transaction();
-    // const [sale] = PublicKey.findProgramAddressSync(
-    //   [Buffer.from('token_sale'), tokenMint.toBuffer()],
-    //   PROGRAM_ID,
-    // );
     const [pda] = PublicKey.findProgramAddressSync([Buffer.from('token_sale')], PROGRAM_ID);
-    console.log('🚀 ~ buyTokens ~ pda:', pda);
+    console.log('🚀 ~ buyTokens ~ pda:', pda.toBase58());
     const [saleSellTokenAccount] = PublicKey.findProgramAddressSync(
       [pda.toBuffer(), TOKEN_PROGRAM_ID.toBuffer(), tokenMint.toBuffer()],
       ASSOCIATED_TOKEN_PROGRAM_ID,
@@ -383,10 +379,6 @@ export const buyTokens = async (mint, buyAmount) => {
       [pda.toBuffer(), TOKEN_PROGRAM_ID.toBuffer(), USDT_MINT.toBuffer()],
       ASSOCIATED_TOKEN_PROGRAM_ID,
     );
-    // const [saleSellTokenAccount] = PublicKey.findProgramAddressSync(
-    //   [sale.toBuffer(), TOKEN_PROGRAM_ID.toBuffer(), tokenMint.toBuffer()],
-    //   ASSOCIATED_TOKEN_PROGRAM_ID,
-    // );
     const cpSwapProgram = new PublicKey('CPMDWBwJDtYax9qW7AyRuVC19Cc4L4Vcy4n2BHAbHkCW');
     const configAddress = new PublicKey('9zSzfkYy6awexsHvmggeH36pfVUdDGyCcwmjT3AQPBj6');
 
